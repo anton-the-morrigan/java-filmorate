232d093c85e4244f188084da9905df35750df380
anton-the-morrigan <anton.the.morrigan@yandex.ru>
2025-02-22 03:14:25 +0300

    Sprint 10

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,73 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ConditionsNotMetException;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.model.Film;
 
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.Map;
+import java.util.HashMap;
+
+@Slf4j
 @RestController
+@RequestMapping("/film")
 public class FilmController {
+
+    private final Map<Integer, Film> films = new HashMap<>();
+    Integer filmId = 0;
+
+    @PostMapping
+    public Film addFilm(@RequestBody Film film) {
+        filmValidator(film);
+        film.setId(filmId);
+        filmId++;
+        films.put(film.getId(), film);
+        return film;
+    }
+
+    @PatchMapping
+    public Film updateFilm(@RequestBody Film newFilm) {
+        if (newFilm.getId() == null) {
+            throw new ConditionsNotMetException("Id должен быть указан");
+        }
+        if (films.containsKey(newFilm.getId())) {
+            Film oldFilm = films.get(newFilm.getId());
+            filmValidator(newFilm);
+            oldFilm.setName(newFilm.getName());
+            oldFilm.setDescription(newFilm.getDescription());
+            oldFilm.setReleaseDate(newFilm.getReleaseDate());
+            oldFilm.setDuration(oldFilm.getDuration());
+            log.info("Данные фильма обновлены");
+            return oldFilm;
+        }
+        throw new NotFoundException(String.format("Фильм с id %d не найден", newFilm.getId()));
+    }
+
+    @GetMapping
+    public Collection<Film> showAllFilms() {
+        return films.values();
+    }
+
+    void filmValidator(Film film) {
+        if (film.getName() == null || film.getName().isBlank()) {
+            throw new ConditionsNotMetException("Название не может быть пустым");
+        }
+        log.info("Выбрано название фильма: {}", film.getName());
+        if (film.getDescription().length() > 200) {
+            throw new ConditionsNotMetException("Длина описания не может превышать 200 символов");
+        }
+        log.info("Выбрано описание фильма: {}", film.getDescription());
+        if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+            throw new ConditionsNotMetException("Дата релиза не может быть раньше 28 декабря 1895 года");
+        }
+        log.info("Выбрана дата релиза фильма: {}", film.getReleaseDate());
+        if (film.getDuration().isNegative()) {
+            throw new ConditionsNotMetException("Продолжительность фильма должна быть положительным числом");
+        }
+        log.info("Выбрана продолжительность фильма: {}", film.getDuration());
+    }
+
 }
---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,73 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ConditionsNotMetException;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    private final Map<Integer, User> users = new HashMap<>();
+    Integer userId = 0;
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        userValidator(user);
+        user.setId(userId);
+        userId++;
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    @PatchMapping
+    public User updateUser(@RequestBody User newUser) {
+        if (newUser.getId() == null) {
+            throw new ConditionsNotMetException("Id должен быть указан");
+        }
+        if (users.containsKey(newUser.getId())) {
+            User oldUser = users.get(newUser.getId());
+            userValidator(newUser);
+            oldUser.setEmail(newUser.getEmail());
+            oldUser.setLogin(newUser.getLogin());
+            oldUser.setName(newUser.getName());
+            oldUser.setBirthday(newUser.getBirthday());
+            log.info("Данные пользователя обновлены");
+            return oldUser;
+        }
+        throw new NotFoundException(String.format("Пользователь с id %d не найден", newUser.getId()));
+    }
+
+    @GetMapping
+    public Collection<User> showAllUsers() {
+        return users.values();
+    }
+
+    void userValidator(User user) {
+        if (!user.getEmail().contains("@") || user.getEmail().isBlank()) {
+            throw new ConditionsNotMetException("Электронная почта не может быть пустой и должна содержать символ");
+        }
+        log.info("Пользователь ввёл электронную почту: {}", user.getEmail());
+        if (user.getLogin().contains(" ") || user.getLogin().isBlank()) {
+            throw new ConditionsNotMetException("логин не может быть пустым и содержать пробел");
+        }
+        log.info("Пользователь ввёл логин: {}", user.getLogin());
+        if (user.getName().isBlank()) {
+            user.setName(user.getLogin());
+            log.info("Пользователь не ввёл имя, поэтому вместо него используется логин");
+        }
+        log.info("Пользователь ввёл имя: {}", user.getName());
+        if (user.getBirthday().isAfter(LocalDate.now())) {
+            throw new ConditionsNotMetException("Дата рождения не может быть в будущем");
+        }
+        log.info("Пользователь ввёл дату рождения: {}", user.getBirthday());
+    }
+}
