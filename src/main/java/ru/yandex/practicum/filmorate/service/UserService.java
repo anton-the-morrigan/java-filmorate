e3e02bf10a6b5497a7432cffc3b96b67877ffe24
anton-the-morrigan <anton.the.morrigan@yandex.ru>
2025-04-20 16:20:51 +0300

    Update UserService.java

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -21,26 +21,13 @@ public class UserService {
     }
 
     public void addFriend(Long id, Long friendId) {
-        if (!userStorage.getUsers().containsKey(id)) {
-            throw new NotFoundException(String.format("Пользователь с id %d не найден", id));
-        }
-        if (!userStorage.getUsers().containsKey(friendId)) {
-            throw new NotFoundException(String.format("Пользователь с id %d не найден", friendId));
-        }
-
-        userStorage.getUsers().get(id).getFriends().add(friendId);
+        userStorage.showUser(id).getFriends().add(friendId);
         log.info(String.format("Пользователь %d теперь дружит с пользователем %d", id, friendId));
-        userStorage.getUsers().get(friendId).getFriends().add(id);
+        userStorage.showUser(friendId).getFriends().add(id);
         log.info(String.format("Пользователь %d теперь дружит с пользователем %d", friendId, id));
     }
 
     public void removeFriend(Long id, Long friendId) {
-        if (!userStorage.getUsers().containsKey(id)) {
-            throw new NotFoundException(String.format("Пользователь с id %d не найден", id));
-        }
-        if (!userStorage.getUsers().containsKey(friendId)) {
-            throw new NotFoundException(String.format("Пользователь с id %d не найден", friendId));
-        }
 
         if (!userStorage.getUsers().get(id).getFriends().contains(friendId)) {
             throw new NotFoundException(String.format("Пользователь с id %d не найден в друзьях у пользователя с id %d", id, friendId));
@@ -49,9 +36,9 @@ public class UserService {
             throw new NotFoundException(String.format("Пользователь с id %d не найден в друзьях у пользователя с id %d", friendId, id));
         }
 
-        userStorage.getUsers().get(id).getFriends().remove(friendId);
+        userStorage.showUser(id).getFriends().remove(friendId);
         log.info(String.format("Пользователь %d больше не дружит с пользователем %d", id, friendId));
-        userStorage.getUsers().get(friendId).getFriends().remove(id);
+        userStorage.showUser(friendId).getFriends().remove(id);
         log.info(String.format("Пользователь %d больше не дружит с пользователем %d", friendId, id));
     }
 
@@ -65,9 +52,9 @@ public class UserService {
 
     public Collection<User> showCommonFriends(Long id, Long otherId) {
         Collection<User> commonFriends = new ArrayList<>();
-        for (Long friend : userStorage.getUsers().get(id).getFriends()) {
-            if (userStorage.getUsers().get(otherId).getFriends().contains(friend)) {
-                commonFriends.add(userStorage.getUsers().get(friend));
+        for (Long friend : userStorage.showUser(id).getFriends()) {
+            if (userStorage.showUser(otherId).getFriends().contains(friend)) {
+                commonFriends.add(userStorage.showUser(friend));
             }
         }
         return commonFriends;
